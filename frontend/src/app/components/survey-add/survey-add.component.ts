daec86895790ef7ec112e36aa7fcf99940abc19a
re2one <rbouarar@hotmail.de>
2020-09-06 12:49:18 +0200

    Adds Component in between survey form and view tree to be able to use it for post and put purposes

---
+++ frontend/src/app/components/survey-add/survey-add.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Output} from '@angular/core';
+import {HttpResponse} from '@angular/common/http';
+import {SurveyResponse} from '../../models/survey';
+import {Router} from '@angular/router';
+import {SurveysService} from '../../services/surveys.service';
+import {EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-survey-add',
+  templateUrl: './survey-add.component.html',
+  styleUrls: ['./survey-add.component.css']
+})
+export class SurveyAddComponent implements OnInit {
+  constructor(
+    public router: Router,
+    private surveysService: SurveysService,
+    ) { }
+
+  ngOnInit(): void {
+  }
+  onSurveySubmit(surveyData): void{
+    this.surveysService.postSurvey(
+      surveyData.title,
+      surveyData.summary,
+      surveyData.description,
+      surveyData.disclaimer
+    ).subscribe((response: HttpResponse<SurveyResponse>) => {
+      console.log(response);
+      if (response.status === 200) {
+        this.router.navigate(['/surveys']);
+      }
+    });
+  }
+}
