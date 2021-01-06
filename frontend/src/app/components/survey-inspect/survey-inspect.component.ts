24d52da09eb9db9c106c653c7059370680acd2b8
re2one <rbouarar@hotmail.de>
2021-01-06 03:35:55 +0100

    retrieves the puzzlepieces in a halfway acceptable manner.

--- frontend/src/app/components/survey-inspect/survey-inspect.component.ts
+++ frontend/src/app/components/survey-inspect/survey-inspect.component.ts
@@ -4,6 +4,7 @@ import {UserService} from '../../services/user.service';
 import {SmolUser} from '../../models/smoUsers';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {QuestionsService} from '../../services/questions.service';
+import {Puzzlepiece} from '../../models/puzzle';
 
 @Component({
   selector: 'app-survey-inspect',
@@ -14,7 +15,10 @@ export class SurveyInspectComponent implements OnInit {
 
   surveyId: string;
   users: Array<SmolUser>;
+  currentUsersAnswers: Map<any, any>;
+  currentQuestions: Array<string>;
   public userForm: FormGroup;
+  public questionForm: FormGroup;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -26,6 +30,9 @@ export class SurveyInspectComponent implements OnInit {
     this.userForm = this.formBuilder.group({
       email: ['', [Validators.required]]
     });
+    this.questionForm = this.formBuilder.group({
+      question: ['', [Validators.required]]
+    });
   }
 
   ngOnInit(): void {
@@ -35,7 +42,6 @@ export class SurveyInspectComponent implements OnInit {
       this.userService.getAll().subscribe((response) => {
         if (response.status === 200) {
           this.users = response.body;
-          // console.log(this.users);
           this.cdr.detectChanges();
         }
       });
@@ -45,9 +51,25 @@ export class SurveyInspectComponent implements OnInit {
   onUserFormSubmit(userEmail): void {
     this.questionsService.getAnsweredQuestions(userEmail.email).subscribe(response => {
       if (response.status === 200) {
-        console.log(response);
+        const map = new Map<string, Array<Puzzlepiece>>();
+        this.currentQuestions = Object.keys(response.body.submissions);
+        Object.keys(response.body.submissions).forEach(key => {
+          const arr = new Array<Puzzlepiece>();
+          response.body.submissions[key].forEach(piece => {
+            const p = new Puzzlepiece();
+            p.Position = piece.position;
+            p.Tapped = piece.tapped;
+            p.Image = piece.image;
+            arr.push(p);
+          });
+          map.set(key, arr);
+        });
+        this.currentUsersAnswers = map;
       }
     });
   }
 
+  onSubmissionSubmit(value): void {
+    console.log(this.currentUsersAnswers.get(value.question));
+  }
 }
