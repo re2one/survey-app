25cd508d205e906059aea145b3927cef452e5aa0
re2one <rbouarar@hotmail.de>
2021-01-14 21:42:24 +0100

    fixing typo

--- backend/model/survey.go
+++ backend/model/survey.go
@@ -6,7 +6,7 @@ import (
 
 type Survey struct {
 	gorm.Model
-	Title        string `json:"title" gorm:"unique`
+	Title        string `json:"title" gorm:"unique"`
 	Summary      string `json:"summary" gorm:"type:text"`
 	Disclaimer   string `json:"disclaimer" gorm:"type:text"`
 	Introduction string `json:"introduction"`
